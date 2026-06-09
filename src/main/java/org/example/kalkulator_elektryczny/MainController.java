71fb088f19a67c5f5da2ddb10283e7f88b617355
SiGMono <szymala777@gmail.com>
2026-06-09 17:57:52 +0200

    Finished hello-view.fxml

--- src/main/java/org/example/kalkulator_elektryczny/MainController.java
+++ src/main/java/org/example/kalkulator_elektryczny/MainController.java
@@ -1,17 +1,41 @@
 package org.example.kalkulator_elektryczny;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController {
 
+    @FXML
     private KalkulatorElektryczny kalkulatorElektryczny;
+    @FXML
+    private ComboBox typComboBox;
+    @FXML
+    private Label pole1label;
+    @FXML
+    private TextField pole1;
+    @FXML
+    private Label pole2label;
+    @FXML
+    private TextField pole2;
+    @FXML
+    private Label pole3label;
+    @FXML
+    private TextField pole3;
+    @FXML
+    private Label wynikLabel;
 
     @FXML
-    private Label welcomeText;
+    public void initialize(){}
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onTypObliczeniaChanged(){
+
+    }
+
+    @FXML
+    public void onObliczClick(){
+        wynikLabel.setText("");
     }
 }
